31f6ec8efb66b8edf49e8443cc0aa312f606f71d
Riad Gozim <r.gozim@dundee.ac.uk>
2017-12-06 09:37:40 +0000

    Added dialog to dispaly additional table row data.

--- js/itr.js
+++ js/itr.js
@@ -6,61 +6,43 @@ $(document).ready(function () {
         tool.logo = mapLogos(tool.name);
     });
 
+    $("#tools").on("click", "tr .detail a", function () {
+        const toolId = $(this).data('id');
+        const toolModel = TOOLS.find(function (tool) {
+            return tool.name === toolId;
+        })
+
+        // Formulate dataset table
+        var tableOptions = {
+            data: toolModel.datasets,
+            lengthChange: false,
+            searching: false,
+            columns: [
+                { data: "name", title: "Dataset" },
+                { data: "availability", title: "Availability" }
+            ]
+        };
+
+        new ToolModal($('#projectModal'))
+            .title(toolModel.name)
+            .projectUrl(toolModel.projectUrl)
+            .biseToolUrl(toolModel.biseToolUrl)
+            .table(tableOptions)
+            .show();
+
+        // $('#projectModal').data("id", attrs).foundation('open');
+    });
+
     $('#tools').DataTable({
         data: TOOLS,
         columns: [
-            {
-                data: "biseToolUrl",
-                visible: false
-            },
-            {
-                data: "logo",
-                visible: false
-            },
-            {
-                data: "name",
-                title: "Name",
-                render: function (data, type, row) {
-                    let div ='<div>';
-                    if (row.logo) {
-                        div += '<img class="itrLogo" src="' + row.logo + '" width="32px"></img>';
-                    }
-                    div += '<a href="' + row.projectUrl + '">' + data + '</a>';
-                    div += '</div>';
-                    return div;
-                }
-            },
-            {
-                data: "projectUrl",
-                title: "Project",
-                visible: false
-            },
-            {
-                data: "domains",
-                title: "Domains",
-                render: function (data, type, row) {
-                    let text = "";
-                    data.forEach(function (domain) {
-                        text += ('<div>' + domain + '</div>');
-                    });
-                    return text;
-                }
-            },
-            {
-                data: "tags",
-                title: "Tags",
-                render: function (data, type, row) {
-                    let text = "";
-                    data.forEach(function (tag) {
-                        text += ('<div><a href="' + tag.url + '">' + tag.name + '</a></div>');
-                    });
-                    return text;
-                }
-            },
-            {
-                data: "datasets",
-                title: "Datasets"
-            }
+            { data: "biseToolUrl", visible: false },
+            { data: "logo", visible: false },
+            { data: "projectUrl", title: "Project", visible: false },
+            { data: "name", title: "Name", render: renderName },
+            { data: "domains", title: "Domains", render: renderDomains },
+            { data: "tags", title: "Tags", render: renderTags },
+            { data: "datasets", title: "Datasets" }
         ],
     });
 
@@ -77,10 +59,74 @@ $(document).ready(function () {
     function mapLogos(toolName) {
         for (let i = 0; i < LOGOS.length; ++i) {
             if (LOGOS[i].name.toLowerCase() === toolName.toLowerCase()) {
-                console.log("Found" + toolName);
                 return logo = LOGOS[i].imgUrl;
             }
         }
         return null;
     }
+
+    function renderName(data, type, row) {
+        let div = '<div class="detail">';
+        if (row.logo) {
+            div += `<img class="itrLogo" src="${row.logo}" width="32px"></img>`;
+        }
+        div += `<a href="#" data-id="${row.name}">${data}</a>`;
+        div += '</div>';
+        return div;
+    }
+
+    function renderDomains(data, type, row) {
+        let text = "";
+        data.forEach(function (domain) {
+            text += ('<div>' + domain + '</div>');
+        });
+        return text;
+    }
+
+    function renderTags(data, type, row) {
+        let text = "";
+        data.forEach(function (tag) {
+            text += ('<div><a href="' + tag.url + '">' + tag.name + '</a></div>');
+        });
+        return text;
+    }
+
+    function renderDialog(row) {
+        // <div class="reveal" id="exampleModal1" data-reveal>
+        // <h1>Awesome. I Have It.</h1>
+        // <p class="lead">Your couch. It is mine.</p>
+        // <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+        // <button class="close-button" data-close aria-label="Close modal" type="button">
+        //   <span aria-hidden="true">&times;</span>
+        // </button>
+        // </div>
+        return null;
+    }
+
+    // Listen to modal opened events
+    // $('#projectModal').on('open.zf.reveal', function () {
+    //     let toolId = $(this).data('id');
+    //     let toolModel = TOOLS.find(function (tool) {
+    //         return tool.name === toolId;
+    //     })
+
+    //     // Formulate dataset table
+    //     var tableOptions = {
+    //         data: toolModel.datasets,
+    //         lengthChange: false,
+    //         searching: false,
+    //         columns: [
+    //             { data: "name", title: "Dataset" },
+    //             { data: "availability", title: "Availability" }
+    //         ]
+    //     };
+
+    //     new ToolModal(this)
+    //         .title(toolModel.name)
+    //         .projectUrl(toolModel.projectUrl)
+    //         .biseToolUrl(toolModel.biseToolUrl)
+    //         .table(tableOptions);
+    // })
+
+
 });
---
+++ js/tool-modal.js
@@ -0,0 +1,154 @@
+var ToolModal = /** @class */ (function () {
+
+    function ToolModal(container) {
+        this.root = container;
+        this.root.append(ToolModal.modalHtml);
+        this.container = this.root.find('#modalContents');
+    }
+
+    ToolModal.prototype.title = function (title) {
+        if (!title) {
+            return this;
+        }
+
+        var header = this.container.find(".modal-header");
+        $(`<h3 class=\"modal-title\">${title}</h3>`)
+            .appendTo(header[0]);
+        return this;
+    };
+
+    ToolModal.prototype.icon = function (url) {
+        var body = this.getBody();
+    }
+
+    ToolModal.prototype.projectUrl = function (url) {
+        if (!url) {
+            return this;
+        }
+
+        var itemsContainer = this.container.find('#itemsContainer');
+        $(`<p>Projet URL: <a href="${url}">${url}</a></p>`)
+            .appendTo(itemsContainer[0]);
+        return this;
+    }
+
+    ToolModal.prototype.biseToolUrl = function (url) {
+        if (!url) {
+            return this;
+        }
+
+        this.container
+            .find('#itemsContainer')[0]
+            .append(`<p>BISE URL: <a href="${url}">${url}</a></p>`);
+        // $(`<p>BISE URL: <a href="${url}">${url}</a></p>`)
+        //     .appendTo(itemsContainer[0]);
+        return this;
+    }
+
+    ToolModal.prototype.table = function (dataTable) {
+        let item = this.container.find('#datasets');
+        $(item[0]).DataTable(dataTable);
+        return this;
+    }
+
+    ToolModal.prototype.message = function (message) {
+        var body = this.getBody();
+        body.text(message);
+        return this;
+    };
+
+    ToolModal.prototype.negativeButton = function (labal, callback) {
+        var _this = this;
+        var footer = this.getFooter();
+        var button = footer.find("#" + ConfirmModal.negativeButtonId);
+        if (button.length === 0) {
+            button = $("<button type=\"button\" class=\"btn btn-default\" id=\"" + ConfirmModal.negativeButtonId + "\"></button>");
+            button.appendTo(footer);
+        }
+        button.text(labal);
+        button.on("click", function (e) {
+            callback(_this, e);
+        });
+        return this;
+    };
+
+    ToolModal.prototype.positiveButton = function (labal, callback) {
+        var _this = this;
+        var footer = this.getFooter();
+        var button = footer.find("#" + ConfirmModal.postiveButtonId);
+        if (button.length === 0) {
+            button = $("<button type=\"button\" class=\"btn btn-default\" id=\"" + ConfirmModal.postiveButtonId + "\"></button>");
+            button.appendTo(footer);
+        }
+        button.text(labal);
+        button.on("click", function (e) {
+            callback(_this, e);
+        });
+        return this;
+    };
+
+    ToolModal.prototype.show = function () {
+        // Add the html to document
+        // this.container.appendTo(this.root ? this.root : $(document));
+        this.root.foundation('open');
+        this.root.on('closed.zf.reveal', () => {
+            if ($(document).find(this.container)) {
+                this.container.remove();
+            }
+        });
+        return this;
+    };
+
+    ToolModal.prototype.dismiss = function () {
+        this.root.foundation('close');;
+    };
+
+    ToolModal.prototype.getHeader = function () {
+        if (!this.header) {
+            var header = $("<div class=\"modal-header\"></div>");
+            header.appendTo($(this.content));
+            this.header = header[0];
+        }
+        return $(this.header);
+    };
+
+    ToolModal.prototype.getBody = function () {
+        let body = this.container.find(".modal-body");
+        if (!body) {
+            body = $("<div class=\"modal-body\"></div>");
+            body.appendTo(this.container);
+        }
+        return body;
+    };
+
+    ToolModal.prototype.getFooter = function () {
+        if (!this.footer) {
+            var footer = $("<div class=\"modal-footer\"></div>");
+            footer.appendTo($(this.content));
+            this.footer = footer[0];
+        }
+        return $(this.footer);
+    };
+
+    ToolModal.modalHtml = `
+        <div id="modalContents">
+            <div class="modal-header">
+                <h3 class="modal-title"></h3>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div id="iconContainer" class="small-2 columns">
+                    </div>
+                    <div id="itemsContainer" class="small-10 columns">
+                    </div>
+                </div>
+                <div id="tableContainer" class="row">
+                    <table id="datasets" class="hover responsive" cellspacing="0" width="100%">
+                    </table>
+                </div>
+            </div>
+        </div>`;
+    ToolModal.postiveButtonId = "postive";
+    ToolModal.negativeButtonId = "negative";
+    return ToolModal;
+}());
