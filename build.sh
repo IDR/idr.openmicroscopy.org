da0dc0fe90726ddf74e5e60d51f21bc74fd9dfec
Sebastien Besson <seb.besson@gmail.com>
2018-08-01 16:12:56 +0100

    Add minimal shebang and shell options

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
+#! /bin/sh
+# Script to build the idr.openmicroscopy.org website using Docker
+
+set -e
+set -u
+
 docker run -it --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/jekyll jekyll build
 docker run -it  --rm -v $PWD/_site:/site jekyll/builder /usr/gem/bin/htmlproofer /site --disable-external --url-ignore "/jupyter,/webclient/"
