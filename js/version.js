46271c209255eee6cbc8a762eb3d3faa71c0b1ee
Sebastien Besson <seb.besson@gmail.com>
2018-11-28 12:25:35 +0000

    Use releases.csv for footer version

--- js/version.js
+++
@@ -1,7 +0,0 @@
----
----
-$(document).ready(function () {
-    $.get('{{ site.baseurl }}/VERSION', null, function(data, textStatus) {
-        $('#version-number-display').text(data);
-    }, 'text');
-});
