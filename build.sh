f1d19313f571a09244761e83370c80266b90ef47
Sebastien Besson <seb.besson@gmail.com>
2019-05-28 22:12:18 +0100

    Re-activate htmlproofer for external links

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 set -u
 
 docker run -it --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:pages jekyll build --config _config.yml,_prod.yml
-docker run -it --rm -v $PWD/_site:/site jekyll/builder:pages /usr/gem/bin/htmlproofer /site --disable-external --url-swap "/about:" --url-ignore "/jupyter,/webclient/"
+docker run -it --rm -v $PWD/_site:/site jekyll/builder:pages /usr/gem/bin/htmlproofer /site --url-swap "/about:" --url-ignore "/jupyter,/webclient/"
