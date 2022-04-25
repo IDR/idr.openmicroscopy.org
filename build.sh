427238d512edd07205aa9a9b4fccdcc65f584c16
Sebastien Besson <seb.besson@gmail.com>
2022-04-25 17:06:00 +0100

    Test using latest Jekyll Docker image for building
    The pages tag ships Jekyll 3.9.0 with Ruby 3.1 which has an issue
    with reading CSV data files

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 set -e
 set -u
 
-docker run --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:pages jekyll build --config _config.yml,_prod.yml
-docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/htmlproofer /site --url-ignore "/jupyter,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/" --only_4xx
+docker run --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:latest jekyll build --config _config.yml,_prod.yml
+docker run --rm -v $PWD/_site:/site/about jekyll/builder:latest /usr/gem/bin/htmlproofer /site --url-ignore "/jupyter,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/" --only_4xx
