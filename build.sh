65719ca710db1b1650cdc073b485eb752c7a34db
Dominik Lindner <d.lindner@dundee.ac.uk>
2024-03-06 09:42:01 +0000

    Ignore broken links for now

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 set -u
 
 docker run --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:pages jekyll build --config _config.yml,_prod.yml
-docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/htmlproofer /site --ignore-urls "/jupyter,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/,/biii.eu/,/localhost/" --only_4xx --no-enforce-https --allow-missing-href
+docker run --rm -v $PWD/_site:/site/about jekyll/builder:pages /usr/gem/bin/htmlproofer /site --ignore-urls "/jupyter,/webclient/,/cell/,/tissue/,/login.binder.bioimagearchive.org/,/biii.eu/,/localhost/" --only_4xx --no-enforce-https --allow-missing-href --ignore-status-codes "400,404"
